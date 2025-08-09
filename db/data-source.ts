dc146bfc0335b0213ab66c3a60296eeadad76c46
Tanvir286 <tanvirdiu200@gmail.com>
2025-08-09 22:46:34 +0600

    setup db

---
+++ db/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import {config}  from 'dotenv';
+config();
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [],
+    migrations: [],
+    logging: false,
+    synchronize: true,
+};
+
+export const dataSource = new DataSource(dataSourceOptions);
